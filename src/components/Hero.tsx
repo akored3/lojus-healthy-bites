c2493bea31c0cd0d2d08f8b5dd3243e9e33b9ec0
JIDE AKINBOLAJI <akinjideakinbolaji@outlook.com>
2026-04-12 23:54:35 +0100

    Pull floating images closer to headline on desktop

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,8 +10,8 @@ function FloatingImage({
 }) {
   const position =
     side === 'left'
-      ? '-left-6 -rotate-6 sm:left-2 md:left-8 lg:left-16'
-      : '-right-6 rotate-6 sm:right-2 md:right-8 lg:right-16'
+      ? '-left-6 -rotate-6 sm:-left-2 md:left-[12%] lg:left-[16%]'
+      : '-right-6 rotate-6 sm:-right-2 md:right-[12%] lg:right-[16%]'
 
   return (
     <div
