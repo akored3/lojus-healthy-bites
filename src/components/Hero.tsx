9d630d79fff7c47c68c846fb1106b5c5d09862f0
JIDE AKINBOLAJI <akinjideakinbolaji@outlook.com>
2026-04-23 23:14:53 +0100

    Drop pink frame and enlarge Hero floating food images

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ function CircleBadge({
   return (
     <div
       aria-hidden="true"
-      className={`pointer-events-none select-none rounded-full border-[3px] border-sky-light bg-white shadow-[6px_6px_0_#1a1a1a] ${className}`}
+      className={`pointer-events-none select-none ${className}`}
     >
       <img src={src} alt="" className="h-full w-full object-contain" />
     </div>
@@ -57,14 +57,14 @@ export function Hero() {
             </div>
           </div>
 
-          <div className="relative mx-auto h-[18rem] w-full max-w-md sm:h-[22rem] md:h-[26rem] lg:h-[30rem]">
+          <div className="relative mx-auto h-[22rem] w-full max-w-md sm:h-[26rem] md:h-[30rem] lg:h-[36rem]">
             <CircleBadge
               src="/images/floating_fruitjuice-removebg-preview.png"
-              className="absolute left-0 top-0 z-0 h-44 w-44 -rotate-6 sm:h-52 sm:w-52 md:h-56 md:w-56 lg:h-64 lg:w-64"
+              className="absolute left-0 top-0 z-0 h-56 w-56 -rotate-6 sm:h-64 sm:w-64 md:h-72 md:w-72 lg:h-80 lg:w-80"
             />
             <CircleBadge
               src="/images/floating_tigernut-removebg-preview.png"
-              className="absolute bottom-0 right-0 z-10 h-44 w-44 rotate-6 sm:h-52 sm:w-52 md:h-56 md:w-56 lg:h-64 lg:w-64"
+              className="absolute bottom-0 right-0 z-10 h-56 w-56 rotate-6 sm:h-64 sm:w-64 md:h-72 md:w-72 lg:h-80 lg:w-80"
             />
           </div>
         </div>
